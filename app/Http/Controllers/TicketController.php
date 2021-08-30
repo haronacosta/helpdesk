5c03670186a9dc4ed89e65d831766e6ecaaac4d3
Haron Acosta <haron.acosta14@gmail.com>
2021-08-30 09:52:16 -0400

    Init project

---
+++ app/Http/Controllers/TicketController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ticket;
+use Illuminate\Http\Request;
+
+class TicketController extends Controller
+{
+    /**
+     * Display a listing of the ticket by user.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = auth()->user()->tickets;
+
+        return response()->json([
+            'success' => true,
+            'data' => $posts
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'subject' => 'required',
+            'description' => 'required'
+        ]);
+
+        $ticket = new Ticket();
+        $ticket->subject = $request->subject;
+        $ticket->description = $request->description;
+
+        if (auth()->user()->tickets()->save($ticket))
+            return response()->json([
+                'success' => true,
+                'data' => $ticket->toArray()
+            ]);
+        else
+            return response()->json([
+                'success' => false,
+                'message' => 'Post not added'
+            ], 500);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $tickets = auth()->user()->tickets()->find($id);
+
+        if (!$tickets) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Post not found '
+            ], 400);
+        }
+
+        return response()->json([
+            'success' => true,
+            'data' => $tickets->toArray()
+        ], 400);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
